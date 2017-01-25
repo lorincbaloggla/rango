9dce60c36ad7ff6aee9c0a7ec6c6a7a4c945b1fc
Lorinc Balog <2274979b@student.gla.ac.uk>
2017-01-25 16:37:31 +0000

    Modified index view and template to show categories Updated categoy model to handle slug Created category template Mapped the urls to the categories Changed index to show dynamic content

--- rango/models.py
+++ rango/models.py
@@ -1,15 +1,22 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.template.defaultfilters import slugify
 
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
+    slug = models.SlugField(unique=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
 
     class Meta:
-        verbose_name_plural = 'Categories'
+        # Guess it's a bug in the book to change this to start with lower case
+        verbose_name_plural = 'categories'
 
     def __str__(self): # For Python 2, use __unicode__ too
         return self.name
@@ -22,9 +29,9 @@ class Page(models.Model):
     title = models.CharField(max_length=128)
     url = models.URLField()
     views = models.IntegerField(default=0)
-    
+
     def __str__(self): # For Python 2, use __unicode__ too
         return self.title
-    
+
     def __unicode__(self):
         return self.title
