8b86fb4a96478a6f4bf767dee194afc8de6d4178
Lorinc Balog <2274979b@student.gla.ac.uk>
2017-02-02 21:25:21 +0000

    Typo corrected.

--- rango/views.py
+++ rango/views.py
@@ -209,7 +209,7 @@ def user_login(request):
                 return HttpResponse("Your Rango account is disabled")
         else:
             print("Invalid login details: {0}, {1}".format(username, password))
-            return HttpResponse("Invalid login detalis supplied.")
+            return HttpResponse("Invalid login details supplied.")
     else:
         return render(request, 'rango/login.html', {})
 
